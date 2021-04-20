0fddfe3a5292920a20984c68ecc0a3cbe7767827
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 01:32:57 -0400

    adding fontawesome icon

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,7 +4,10 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <h1>Navbar</h1>
+        <h1>
+          <i className="fab fa-github" />
+          Navbar
+        </h1>
       </div>
     );
   }
