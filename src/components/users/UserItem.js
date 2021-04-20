378258f8cfe0a0a5973b8e10293beb718766fcd2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 15:48:28 -0400

    deleted the state and instead passed the props.user from the user component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'mojombo',
-    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
+  // state = {
+  //   id: 'id',
+  //   login: 'mojombo',
+  //   avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+  //   html_url: 'https://github.com/mojombo',
+  // };
 
   render() {
-    const { avatar_url, login, html_url } = this.state;
+    const { avatar_url, login, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
