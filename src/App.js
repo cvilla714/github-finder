2e846fa1ea977855d802d36ff8a5e03e3ffc1a90
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 01:20:11 -0400

    imported navbar and render it to the DOM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
 
 class App extends React.Component {
   render() {
-    const name = 'Cosmel Villalobos';
     return (
       <div className="App">
-        <h1>hello from {name}</h1>
+        <Navbar />
       </div>
     );
   }
