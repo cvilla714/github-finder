ad5c18d2ae393a4cff0cb151dda8c18e9414b5b3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 12:46:38 -0400

    got ride of the constructor and super and left only the state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  };
 
   render() {
     return (
