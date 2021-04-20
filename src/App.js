3d94e628b7822df5c72a0ca1122e49f16c333a49
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 16:03:47 -0400

    added a container div to wrap the Users components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
-        <Users />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
