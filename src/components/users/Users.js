c84dde28b89c7ae8646770ccc0209a474a5cb4db
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 16:03:25 -0400

    added style to a div and passed the values to a variable holding the properties as objects

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -26,7 +26,7 @@ export class Users extends Component {
   };
   render() {
     return (
-      <div>
+      <div style={userStyle}>
         {this.state.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
@@ -35,4 +35,9 @@ export class Users extends Component {
   }
 }
 
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3,1fr)',
+  gridGapo: '1rem',
+};
 export default Users;
