981e843902fb6133da98e4734c2e2038538d6f68
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 12:43:58 -0400

    added the correct class to the navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -13,12 +13,12 @@ export class Navbar extends Component {
   };
   render() {
     return (
-      <div>
+      <nav className="navbar bg-primary">
         <h1>
           <i className={this.props.icon} />
           {this.props.title}
         </h1>
-      </div>
+      </nav>
     );
   }
 }
