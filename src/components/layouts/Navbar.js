08bee0f6cecccee1e2781c098ab89804ef409a58
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 01:54:06 -0400

    adding default props and proptypes

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fa fa-github',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
   render() {
     return (
       <div>
         <h1>
-          <i className="fab fa-github" />
-          Navbar
+          <i className={this.props.icon} />
+          {this.props.title}
         </h1>
       </div>
     );
