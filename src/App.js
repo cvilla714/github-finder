1e90427119161af918015e83cc50530851894a49
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-20 01:08:53 -0400

    passing a variable value

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 class App extends React.Component {
   render() {
+    const name = 'Cosmel Villalobos';
     return (
       <div className="App">
-        <h1>hello from react</h1>
+        <h1>hello from {name}</h1>
       </div>
     );
   }
