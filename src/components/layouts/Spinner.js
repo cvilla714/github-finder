83a57d6c8dd11e6dec1c974cde6347c0e6f62fe9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-21 13:22:04 -0400

    added the Spinner component and added style

---
+++ src/components/layouts/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import spinner from './spinner.gif';
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} alt="Loading" style={{ width: '200px', marging: 'auto', display: 'block' }} />
+    </div>
+  );
+};
+
+export default Spinner;
