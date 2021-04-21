db8ef6d6869446244fdeac5989f7adbb66b8a4d5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-21 19:02:44 -0400

    added the onSubmit and onChange events

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    text: '',
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    // console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = (e) => {
+    // this.setState({ text: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
       <div>
-        <form className="form">
-          <input type="text" name="text" placeholder="Search Users..." />
+        <form onSubmit={this.onSubmit} className="form">
+          <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
       </div>
