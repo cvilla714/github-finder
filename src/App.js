d8c948263135e555f416451e4ddc1807d3094fea
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-21 01:28:13 -0400

    imported axios and use it to perform the get request form the api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import Navbar from './components/layouts/Navbar';
 // import UserItem from './components/users/UserItem';
 import Users from './components/users/Users.js';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://api.github.com/users').then((res) => console.log(res.data));
+  }
   render() {
     return (
       <div className="App">
