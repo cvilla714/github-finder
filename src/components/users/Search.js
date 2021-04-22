3d9c9118c5f30f64f81d72d74818c25aa392a461
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-22 01:15:42 -0400

    added a condition to show up the alert if the state of the text is equla to null

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,12 +9,17 @@ class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
   onSubmit = (e) => {
     e.preventDefault();
     // console.log(this.state.text);
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter a valid name', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   onChange = (e) => {
