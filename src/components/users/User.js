db3741f225985aacf15149745f006a34d873dfa2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-22 17:36:05 -0400

    added the username, website and company

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -14,7 +14,7 @@ class User extends Component {
     getUser: PropTypes.func.isRequired,
   };
   render() {
-    const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user;
+    const { name, company, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user;
 
     const { loading } = this.props;
 
@@ -42,7 +42,38 @@ class User extends Component {
             <a href={html_url} className="btn btn-dark my1">
               Visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary"></div>
         </div>
       </Fragment>
     );
