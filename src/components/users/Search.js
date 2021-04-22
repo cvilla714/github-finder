15c0ed08bb5acbeb899bf2e93cc22a6b808b7b55
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-21 23:43:27 -0400

    added the showClear button and destruturing

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,7 @@ class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
   onSubmit = (e) => {
     e.preventDefault();
@@ -21,15 +22,18 @@ class Search extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
-        <button className="btn-light btn-block" onClick={this.props.clearUsers}>
-          Clear
-        </button>
+        {showClear && (
+          <button className="btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
