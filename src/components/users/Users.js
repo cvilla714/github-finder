7c21485298ea0687867bf9297d80cbeb8e07177f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-24 01:37:35 -0400

    eliminated proptypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layouts/Spinner';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
 
 const Users = () => {
