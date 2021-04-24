0ccb9e85df9cc973aaaed1eebb3f125d2961ec56
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-24 01:38:35 -0400

    added the getUserRepos and also added the function inside provider

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -52,6 +52,22 @@ const GithubState = (props) => {
   };
 
   //Get Repost
+  const getUserRepos = async (username) => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`, {
+      headers: {
+        Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    // setRepos(res.data);
+    // setLoading(false);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
 
   //Clear users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -70,6 +86,7 @@ const GithubState = (props) => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
