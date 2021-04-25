f7596a138a513947233ca80c669a9da8b5618b55
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-24 22:04:58 -0400

    imported the AlertContext and initialized it

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,17 @@
 import React, { useContext, useState } from 'react';
-import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
 const Search = ({ setAlert }) => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
 
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter a valid name', 'light');
+      alertContext.setAlert('Please enter a valid name', 'light');
     } else {
       githubContext.searchUsers(text);
       setText('');
@@ -34,7 +35,4 @@ const Search = ({ setAlert }) => {
   );
 };
 
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired,
-};
 export default Search;
