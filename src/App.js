dcaac8992dc2e6f43cd5a28ae61c322940a90cf3
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-24 22:03:16 -0400

    deleted all commented code and wrap the component inside the AlertState

--- src/App.js
+++ src/App.js
@@ -1,97 +1,32 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
-import Users from './components/users/Users.js';
 import User from './components/users/User.js';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layouts/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //   this.setState({ users: res.data, loading: false });
-  // }
-
-  // const searchUsers = async (text) => {
-  //   setLoading(true);
-
-  //   const res = await axios.get(`https://api.github.com/search/users?q=${text}`, {
-  //     headers: {
-  //       Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
-  //     },
-  //   });
-
-  //   setUsers(res.data.items);
-  //   setLoading(false);
-  // };
-
-  //Get single Github User
-  // const getUser = async (username) => {
-  //   setLoading(true);
-
-  //   const res = await axios.get(`https://api.github.com/users/${username}`, {
-  //     headers: {
-  //       Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
-  //     },
-  //   });
-
-  //   setUser(res.data);
-  //   setLoading(false);
-  // };
-
-  //Get users repositories
-  // const getUserRepos = async (username) => {
-  //   setLoading(true);
-
-  //   const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`, {
-  //     headers: {
-  //       Authorization: `${process.env.REACT_APP_GITHUB_TOKEN}`,
-  //     },
-  //   });
-
-  //   setRepos(res.data);
-  //   setLoading(false);
-  // };
-
-  //Clear Users from state
-
-  // set up the alert
-  const showAlert = (message, type) => {
-    setAlert({ message: message, type: type });
-
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              ></Route>
-              <Route exact path="/about" component={About} />
-              <Route exact path="/user/:login" component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
